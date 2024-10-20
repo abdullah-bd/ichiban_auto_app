0927a9c43231fc9b3a5da8388eb1138ee8194ec8
Md Abdullah <spark8ab@gmail.com>
2024-10-20 17:32:26 +0600

    registration view added.

--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -6,6 +6,7 @@ abstract class Routes {
   static const HOME = _Paths.HOME;
   static const AUTH = _Paths.AUTH;
   static const CREATE_BOOKING = _Paths.CREATE_BOOKING;
+  static const REGISTRATION = _Paths.REGISTRATION;
 }
 
 abstract class _Paths {
@@ -13,4 +14,5 @@ abstract class _Paths {
   static const HOME = '/home';
   static const AUTH = '/auth';
   static const CREATE_BOOKING = '/create-booking';
+  static const REGISTRATION = '/registration';
 }
